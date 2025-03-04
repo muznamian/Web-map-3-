13c55b3c99431baf58decd7d8cc04f0cbc53942c
muznamian <muzna.mian@mail.utoronto.ca>
2025-03-04 14:48:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibXV6bmFtaWFuIiwiYSI6ImNtNXBsc2xjcDAyaWkybm9wZXFuMjNzMTQifQ.0ATJsQJDSlSrpNrQpdMq0Q';
-// the specific token that is used in my mapbox file
 const map = new mapboxgl.Map({
-    container: "my-map 3",
+    container: "my-map", // was important to correct the ID name (had it as 'my-map 3' but changed to 'my-map')- the html would not work with spaces 
     style: "mapbox://styles/muznamian/cm7uod8ri00du01sagvvh78i8",
-    // the style url from the mapbox id
     center: [-79.39, 43.66],
-    // the lng and lat values that are used as central mercator 
     zoom: 12,
 });
 
 map.on('load', () => {
-
+    // Adding GeoJSON source and fixing the link using GitHUB 
     map.addSource('touristattractions', {
         type: 'geojson',
         data: 'https://raw.githubusercontent.com/muznamian/Web-map-3-/refs/heads/main/touristattractions.geojson'
-    });})
+    });
 
-     //  the above is geojson link and the code actions to plot the point in the map 
-     map.addLayer({
-        'id': 'location points',
+    // Adding the map addlayer inside the 'load' event since when it was outside points would not load 
+    map.addLayer({
+        'id': 'location-points',
         'type': 'circle',
         'source': 'touristattractions',
         'paint': {
             'circle-radius': 6,
             'circle-color': '#FF0000'
         }
-
     });
-// the code above explains the description, as my points are round circles 
-    //map box tile stuff
+});
